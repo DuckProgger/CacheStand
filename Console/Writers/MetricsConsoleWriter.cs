ba10494deccef08bda8bc41926300782c1fb8010
Никитин Виталий <Nikitin_VV@akelon.com>
2023-10-26 18:09:23 +1000

    Рефакторинг

--- Console/Writers/MetricsConsoleWriter.cs
+++ Console/Writers/MetricsConsoleWriter.cs
@@ -1,5 +1,4 @@
 ﻿using Core.Metric;
-using System.Text;
 
 namespace Console.Writers;
 
@@ -7,66 +6,61 @@ public class MetricsConsoleWriter : IMetricsWriter
 {
     public void Write(MetricsResult metricsResult)
     {
-        //System.Console.SetCursorPosition(0, 0);
-        //System.Console.SetWindowSize(1500, 1000);
         System.Console.Clear();
-        //System.Console.WriteLine("\u001b[2J\u001b[3J");
 
-        //StringBuilder x = new StringBuilder().Append(' ', System.Console.WindowHeight * System.Console.WindowWidth);
-        //System.Console.Write('\n' + x.ToString()); 
-        //System.Console.SetCursorPosition(0, 0);
-
-        //for (int i = 0; i < System.Console.BufferHeight; i++)
-        //    System.Console.WriteLine();
+        var percantageMetricsColor = ConsoleColor.DarkYellow;
+        var numberMetricsColor = ConsoleColor.DarkGreen;
+        var timeMetricsColor = ConsoleColor.Cyan;
 
         var tableConsoleWriter = new ConsoleTableConsoleWriter(
             new ConsoleTableConsoleWriter.Column("Parameter", 35),
             new ConsoleTableConsoleWriter.Column("Value", 20));
+        
         tableConsoleWriter.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("Cache efficiency", ConsoleColor.DarkYellow),
-            new ConsoleTableConsoleWriter.Cell($"{metricsResult.CacheEfficiency:.##} %", ConsoleColor.DarkYellow));
+            new ConsoleTableConsoleWriter.Cell("Cache efficiency", percantageMetricsColor),
+            new ConsoleTableConsoleWriter.Cell($"{metricsResult.CacheEfficiency:.##} %", percantageMetricsColor));
         tableConsoleWriter.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("Hit rate", ConsoleColor.DarkYellow),
-            new ConsoleTableConsoleWriter.Cell($"{metricsResult.HitRate:.##} %", ConsoleColor.DarkYellow));
+            new ConsoleTableConsoleWriter.Cell("Hit rate", percantageMetricsColor),
+            new ConsoleTableConsoleWriter.Cell($"{metricsResult.HitRate:.##} %", percantageMetricsColor));
         tableConsoleWriter.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("RPS", ConsoleColor.DarkGreen),
-            new ConsoleTableConsoleWriter.Cell(metricsResult.RequestsPerSecond, ConsoleColor.DarkGreen));
+            new ConsoleTableConsoleWriter.Cell("RPS", numberMetricsColor),
+            new ConsoleTableConsoleWriter.Cell(metricsResult.RequestsPerSecond, numberMetricsColor));
         tableConsoleWriter.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("Total requests", ConsoleColor.DarkGreen),
-            new ConsoleTableConsoleWriter.Cell(metricsResult.TotalRequests, ConsoleColor.DarkGreen));
+            new ConsoleTableConsoleWriter.Cell("Total requests", numberMetricsColor),
+            new ConsoleTableConsoleWriter.Cell(metricsResult.TotalRequests, numberMetricsColor));
         tableConsoleWriter.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("Total read requests", ConsoleColor.DarkGreen),
-            new ConsoleTableConsoleWriter.Cell(metricsResult.TotalReadRequests, ConsoleColor.DarkGreen));
+            new ConsoleTableConsoleWriter.Cell("Total read requests", numberMetricsColor),
+            new ConsoleTableConsoleWriter.Cell(metricsResult.TotalReadRequests, numberMetricsColor));
         tableConsoleWriter.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("Total hits", ConsoleColor.DarkGreen),
-            new ConsoleTableConsoleWriter.Cell(metricsResult.TotalCacheHits, ConsoleColor.DarkGreen));
+            new ConsoleTableConsoleWriter.Cell("Total hits", numberMetricsColor),
+            new ConsoleTableConsoleWriter.Cell(metricsResult.TotalCacheHits, numberMetricsColor));
         tableConsoleWriter.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("Total request time", ConsoleColor.Cyan),
-            new ConsoleTableConsoleWriter.Cell(metricsResult.TotalRequestTime, ConsoleColor.Cyan));
+            new ConsoleTableConsoleWriter.Cell("Total request time", timeMetricsColor),
+            new ConsoleTableConsoleWriter.Cell(metricsResult.TotalRequestTime, timeMetricsColor));
         tableConsoleWriter.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("Average request time", ConsoleColor.Cyan),
-            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageRequestTime.TotalMicroseconds} мкс", ConsoleColor.Cyan));
+            new ConsoleTableConsoleWriter.Cell("Average request time", timeMetricsColor),
+            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageRequestTime.TotalMicroseconds} мкс", timeMetricsColor));
         tableConsoleWriter.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("Average cache costs", ConsoleColor.Cyan),
-            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageRequestTime.TotalMicroseconds} мкс", ConsoleColor.Cyan));
+            new ConsoleTableConsoleWriter.Cell("Average cache costs", timeMetricsColor),
+            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageCacheTime.TotalMicroseconds} мкс", timeMetricsColor));
         tableConsoleWriter.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("Average repository read time", ConsoleColor.Cyan),
-            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageRepositoryReadTime.TotalMicroseconds} мкс", ConsoleColor.Cyan));
+            new ConsoleTableConsoleWriter.Cell("Average repository read time", timeMetricsColor),
+            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageRepositoryReadTime.TotalMicroseconds} мкс", timeMetricsColor));
         tableConsoleWriter.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("Average cache read time", ConsoleColor.Cyan),
-            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageReadCacheTime.TotalMicroseconds} мкс", ConsoleColor.Cyan));
+            new ConsoleTableConsoleWriter.Cell("Average cache read time", timeMetricsColor),
+            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageReadCacheTime.TotalMicroseconds} мкс", timeMetricsColor));
         tableConsoleWriter.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("Average deserialization time", ConsoleColor.Cyan),
-            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageDeserializationTime.TotalMicroseconds} мкс", ConsoleColor.Cyan));
+            new ConsoleTableConsoleWriter.Cell("Average deserialization time", timeMetricsColor),
+            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageDeserializationTime.TotalMicroseconds} мкс", timeMetricsColor));
         tableConsoleWriter.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("Average repository write time", ConsoleColor.Cyan),
-            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageRepositoryWriteTime.TotalMicroseconds} мкс", ConsoleColor.Cyan));
+            new ConsoleTableConsoleWriter.Cell("Average repository write time", timeMetricsColor),
+            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageRepositoryWriteTime.TotalMicroseconds} мкс", timeMetricsColor));
         tableConsoleWriter.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("Average cache write time", ConsoleColor.Cyan),
-            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageWriteCacheTime.TotalMicroseconds} мкс", ConsoleColor.Cyan));
+            new ConsoleTableConsoleWriter.Cell("Average cache write time", timeMetricsColor),
+            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageWriteCacheTime.TotalMicroseconds} мкс", timeMetricsColor));
         tableConsoleWriter.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("Average serialization time", ConsoleColor.Cyan),
-            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageSerializationTime.TotalMicroseconds} мкс", ConsoleColor.Cyan));
+            new ConsoleTableConsoleWriter.Cell("Average serialization time", timeMetricsColor),
+            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageSerializationTime.TotalMicroseconds} мкс", timeMetricsColor));
 
         tableConsoleWriter.Write();
     }
