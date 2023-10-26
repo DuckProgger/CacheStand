0dad18959d379ef0f2dc4fd01dd98a65bf70d4ed
Никитин Виталий <Nikitin_VV@akelon.com>
2023-10-26 15:57:49 +1000

    Цветные строки в таблице

--- Console/Writers/MetricsConsoleWriter.cs
+++ Console/Writers/MetricsConsoleWriter.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Metric;
+using System.Text;
 
 namespace Console.Writers;
 
@@ -6,79 +7,67 @@ public class MetricsConsoleWriter : IMetricsWriter
 {
     public void Write(MetricsResult metricsResult)
     {
+        //System.Console.SetCursorPosition(0, 0);
+        //System.Console.SetWindowSize(1500, 1000);
         System.Console.Clear();
+        //System.Console.WriteLine("\u001b[2J\u001b[3J");
 
-        var test = new ConsoleTableConsoleWriter(
+        //StringBuilder x = new StringBuilder().Append(' ', System.Console.WindowHeight * System.Console.WindowWidth);
+        //System.Console.Write('\n' + x.ToString()); 
+        //System.Console.SetCursorPosition(0, 0);
+
+        //for (int i = 0; i < System.Console.BufferHeight; i++)
+        //    System.Console.WriteLine();
+
+        var tableConsoleWriter = new ConsoleTableConsoleWriter(
             new ConsoleTableConsoleWriter.Column("Parameter", 35),
             new ConsoleTableConsoleWriter.Column("Value", 20));
-        test.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("Total request time"),
-            new ConsoleTableConsoleWriter.Cell(metricsResult.TotalRequestTime));
-        test.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("Average request time"),
-            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageRequestTime.TotalMicroseconds} мкс"));
-        test.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("Average cache costs"),
-            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageRequestTime.TotalMicroseconds} мкс"));
-        test.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("Cache efficiency"),
-            new ConsoleTableConsoleWriter.Cell($"{metricsResult.CacheEfficiency:.##} %"));
-        test.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("Hit rate"),
-            new ConsoleTableConsoleWriter.Cell($"{metricsResult.HitRate:.##} %"));
-        test.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("RPS"),
-            new ConsoleTableConsoleWriter.Cell(metricsResult.RequestsPerSecond));
-        test.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("Total requests"),
-            new ConsoleTableConsoleWriter.Cell(metricsResult.TotalRequests));
-        test.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("Total read requests"),
-            new ConsoleTableConsoleWriter.Cell(metricsResult.TotalReadRequests));
-        test.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("Total hits"),
-            new ConsoleTableConsoleWriter.Cell(metricsResult.TotalCacheHits));
-        test.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("Average repository read time"),
-            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageRepositoryReadTime.TotalMicroseconds} мкс"));
-        test.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("Average cache read time"),
-            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageReadCacheTime.TotalMicroseconds} мкс"));
-        test.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("Average deserialization time"),
-            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageDeserializationTime.TotalMicroseconds} мкс"));
-        test.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("Average repository write time"),
-            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageRepositoryWriteTime.TotalMicroseconds} мкс"));
-        test.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("Average cache write time"),
-            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageWriteCacheTime.TotalMicroseconds} мкс"));
-        test.AppendRow(
-            new ConsoleTableConsoleWriter.Cell("Average serialization time"),
-            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageSerializationTime.TotalMicroseconds} мкс"));
+        tableConsoleWriter.AppendRow(
+            new ConsoleTableConsoleWriter.Cell("Cache efficiency", ConsoleColor.DarkYellow),
+            new ConsoleTableConsoleWriter.Cell($"{metricsResult.CacheEfficiency:.##} %", ConsoleColor.DarkYellow));
+        tableConsoleWriter.AppendRow(
+            new ConsoleTableConsoleWriter.Cell("Hit rate", ConsoleColor.DarkYellow),
+            new ConsoleTableConsoleWriter.Cell($"{metricsResult.HitRate:.##} %", ConsoleColor.DarkYellow));
+        tableConsoleWriter.AppendRow(
+            new ConsoleTableConsoleWriter.Cell("RPS", ConsoleColor.DarkGreen),
+            new ConsoleTableConsoleWriter.Cell(metricsResult.RequestsPerSecond, ConsoleColor.DarkGreen));
+        tableConsoleWriter.AppendRow(
+            new ConsoleTableConsoleWriter.Cell("Total requests", ConsoleColor.DarkGreen),
+            new ConsoleTableConsoleWriter.Cell(metricsResult.TotalRequests, ConsoleColor.DarkGreen));
+        tableConsoleWriter.AppendRow(
+            new ConsoleTableConsoleWriter.Cell("Total read requests", ConsoleColor.DarkGreen),
+            new ConsoleTableConsoleWriter.Cell(metricsResult.TotalReadRequests, ConsoleColor.DarkGreen));
+        tableConsoleWriter.AppendRow(
+            new ConsoleTableConsoleWriter.Cell("Total hits", ConsoleColor.DarkGreen),
+            new ConsoleTableConsoleWriter.Cell(metricsResult.TotalCacheHits, ConsoleColor.DarkGreen));
+        tableConsoleWriter.AppendRow(
+            new ConsoleTableConsoleWriter.Cell("Total request time", ConsoleColor.Cyan),
+            new ConsoleTableConsoleWriter.Cell(metricsResult.TotalRequestTime, ConsoleColor.Cyan));
+        tableConsoleWriter.AppendRow(
+            new ConsoleTableConsoleWriter.Cell("Average request time", ConsoleColor.Cyan),
+            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageRequestTime.TotalMicroseconds} мкс", ConsoleColor.Cyan));
+        tableConsoleWriter.AppendRow(
+            new ConsoleTableConsoleWriter.Cell("Average cache costs", ConsoleColor.Cyan),
+            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageRequestTime.TotalMicroseconds} мкс", ConsoleColor.Cyan));
+        tableConsoleWriter.AppendRow(
+            new ConsoleTableConsoleWriter.Cell("Average repository read time", ConsoleColor.Cyan),
+            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageRepositoryReadTime.TotalMicroseconds} мкс", ConsoleColor.Cyan));
+        tableConsoleWriter.AppendRow(
+            new ConsoleTableConsoleWriter.Cell("Average cache read time", ConsoleColor.Cyan),
+            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageReadCacheTime.TotalMicroseconds} мкс", ConsoleColor.Cyan));
+        tableConsoleWriter.AppendRow(
+            new ConsoleTableConsoleWriter.Cell("Average deserialization time", ConsoleColor.Cyan),
+            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageDeserializationTime.TotalMicroseconds} мкс", ConsoleColor.Cyan));
+        tableConsoleWriter.AppendRow(
+            new ConsoleTableConsoleWriter.Cell("Average repository write time", ConsoleColor.Cyan),
+            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageRepositoryWriteTime.TotalMicroseconds} мкс", ConsoleColor.Cyan));
+        tableConsoleWriter.AppendRow(
+            new ConsoleTableConsoleWriter.Cell("Average cache write time", ConsoleColor.Cyan),
+            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageWriteCacheTime.TotalMicroseconds} мкс", ConsoleColor.Cyan));
+        tableConsoleWriter.AppendRow(
+            new ConsoleTableConsoleWriter.Cell("Average serialization time", ConsoleColor.Cyan),
+            new ConsoleTableConsoleWriter.Cell($"{metricsResult.AverageSerializationTime.TotalMicroseconds} мкс", ConsoleColor.Cyan));
 
-        test.Write();
-        
-        
-        //System.Console.WriteLine($"""
-        //                          Total request time:...{metricsResult.TotalRequestTime}
-        //                          Average request time:.{metricsResult.AverageRequestTime.TotalMicroseconds} мкс
-        //                          Average cache costs:..{metricsResult.AverageCacheTime.TotalMicroseconds} мкс
-        //                          Cache efficiency:.....{metricsResult.CacheEfficiency:.##} %
-        //                          Hit rate:.............{metricsResult.HitRate:.##} %
-        //                          RPS:..................{metricsResult.RequestsPerSecond}
-        //                          Total requests:.......{metricsResult.TotalRequests}
-        //                          Total read requests:..{metricsResult.TotalReadRequests}
-        //                          Total hits:...........{metricsResult.TotalCacheHits}
-        //                          """);
-        //System.Console.WriteLine();
-        //System.Console.WriteLine($"""
-        //                          Average repository read time:..{metricsResult.AverageRepositoryReadTime.TotalMicroseconds} мкс
-        //                          Average cache read time:.......{metricsResult.AverageReadCacheTime.TotalMicroseconds} мкс
-        //                          Average deserialization time:..{metricsResult.AverageDeserializationTime.TotalMicroseconds} мкс
-        //                          Average repository write time:.{metricsResult.AverageRepositoryWriteTime.TotalMicroseconds} мкс
-        //                          Average cache write time:......{metricsResult.AverageWriteCacheTime.TotalMicroseconds} мкс
-        //                          Average serialization time:....{metricsResult.AverageSerializationTime.TotalMicroseconds} мкс
-        //                          """);
+        tableConsoleWriter.Write();
     }
 }
